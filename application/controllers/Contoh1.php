abf0aaeb0b56e9428ebbe0c6ebce66a305b1cb92
adeskriwil <surya.ade@gmail.com>
2021-10-04 10:37:17 +0700

    ubah isi file contoh1.php

--- application/controllers/Contoh1.php
+++ application/controllers/Contoh1.php
@@ -4,9 +4,9 @@ class Contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama saya Imam Nawawi
-            Saya tingga di daerah Ciputat
+        echo "Nama saya Ade Suryanto
+            Saya tinggal di daerah Ciputat,
             olah raga yang saya sukai adalah
-            Bulutangkis pakai raket yonex";
+            Bulutangkis, Futsal dan Renang";
     }
 }
